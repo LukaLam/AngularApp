f538ed7539f1dc4c3fc446065a063e3d77565da2
WarkM <luka.lambalot@hotmail.fr>
2019-11-07 10:54:48 +0100

    ajout de l'incremente sur le compteur 43

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -18,11 +18,18 @@ export class CounterService {
     this.initialValue = [0,0,0];
   }
 
-  increment(position: number): number {
+ /* increment(position: number): number {
     this.initialValue[position]++;
     return this.initialValue[position];
+  } */
+
+  increment(): Observable<Counter>{
+    //this.httpClient.patch<Counter>("https://lp4asgadot.herokuapp.com/counters/2.json",{"value" : 1});
+    this.httpClient.patch("https://lp4asgadot.herokuapp.com/counters/43.json",{"value" : 1}).subscribe();
+    return this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/43.json");
   }
+
   getCounterValue(id: number): Observable<Counter> {
-    return this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+id+".json")
+    return this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+(id+41)+".json")  // comme dans l'autre fichier, il n'y a pas de compteur numéro 3, uniquement 43/44 ou 45, donc j'incrémente le 43eme
   }
 }
